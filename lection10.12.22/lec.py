3cbe148b248c357ed2826b87190f9828292cf1f8
Ramil <ramil-urazgulov444@mail.ru>
2022-12-10 20:27:24 +0700

    primeri

--- lection10.12.22/lec.py
+++ lection10.12.22/lec.py
@@ -1,7 +1,28 @@
-a = 123
-b = 1.13
-s = 'hello world'
-#print(s) #вывод строки
+#a = 123
+#b = 1.13
+#s = 'hello world'
+# print(s) #вывод строки
 #print(f'{a} - {b} - {s}' )
 #print(a, b , s)
 
+#print('Введите число а')
+#a = int(input())
+#print('Введите число b')
+#b = int (input())
+#print(a, '+', b, '=', a+b)
+
+#f = 1<6 or 2<4
+# print(f)
+
+#a = int(input('Введите число а '))
+#b = int(input('Введите число b '))
+
+# if a>b:
+#   print(a)
+# else:
+#   print(b)
+# for i in 1,2,3,4,5:
+#   print(i**2)
+
+for i in range(2, 10,):
+    print(i)
